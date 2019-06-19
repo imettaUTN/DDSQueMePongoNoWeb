952be407899db02a1a8054b319c2ccf1deb2972f
Cristian <Cristian@Cristian-PC>
2019-06-19 01:05:57 -0300

    Se suben cambios en metodos get/set

--- QueMePongoNoWeb/src/TipoPrenda.java
+++ QueMePongoNoWeb/src/TipoPrenda.java
@@ -1,22 +1,15 @@
 import java.util.*;
+import lombok.*;
 
+@Getter
+@Setter
 public class TipoPrenda {
-private List<String> telasValidas = new ArrayList<String>();
-private int nivelAbrigo ;
-
-public void AddTelaValida(String tela) {
-	this.telasValidas.add(tela);	
-}
-
-public int getNivelAbrigo() {
-	return nivelAbrigo;
-}
-public void setNivelAbrigo(int nivelAbrigo) {
-	this.nivelAbrigo = nivelAbrigo;
-}
-
-public boolean IsTelaValida(String tela) {
-	return this.telasValidas.contains(tela);
-}
+	
+	private List<String> telasValidas = new ArrayList<String>();
+	private int nivelAbrigo ;
+	
+	public boolean IsTelaValida(String tela) {
+		return this.telasValidas.contains(tela);
+	}
 
 }
